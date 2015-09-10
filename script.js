db65e95823734c47772cd40210cb2b1d4d026376
alediez2408@gmail.com rqtsw123@ <alediez2408@gmail.com>
2015-09-10 15:07:48 -0500

    Updated alternating x and os

--- script.js
+++ script.js
@@ -6,7 +6,14 @@
 
 // Create a function that lets clicking show output a letter either x or O
 // Add function to alternate between x and o with every players click 
+// add reset button to refresh game
+// create an array of all possible winning combinations
+// Create a function that loops through winning combinations and outputsan alert 
+		//it takes a minimum of 5 moves to win this game, start checking combination after 5 clicks 
+		//if after five clicks ids A1,A2,A3, B1,B2,B3... have been chosen sotp program and alert winner 
 
+
+ 
 
   var playerOne = true; 
 
@@ -20,20 +27,21 @@
   	 		  playerOne=true ;
   	 } 
   }); 
-  	
-  
-  	 
-// });
 
 
-// var playerTwo = false
 
-// $(".container button").click(function() {
-//   $(this).html("<p> O </p>");
+//var winCombo = [(A1,A2,A3),(B1,B2,B3),(C1,C2,C3),(A1,B1,C1),(A2,B2,C2),(A3,B3,C3),(A1,B2,C3),(A3,B2,C1)]
+
+  // function determineWinner {
+  // 		for (var i = 0; i < 3; i++)
+
+
+  // }
+
+
  
-// });
-
-
+ 
+  
 
 
 
